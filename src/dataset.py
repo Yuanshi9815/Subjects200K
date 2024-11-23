ec80c865d12ec5aa40aec12d3bb448076e3f7bf6
Yuanshi9815 <Yuanshi9815@outlook.com>
2024-11-23 17:06:49 +0800

    mv files and make the code concise

--- src/dataset.py
+++ src/dataset.py
@@ -24,117 +24,24 @@ def get_file(path):
         raise Exception(f"Failed to download file {file_path}")
 
 
-class Split1(Dataset):
+class SplitBase(Dataset):
     def __init__(
         self,
         path: str,
         condition_max_size: int = 512,
         enable_filter: bool = True,
+        prefix: str = "output",
     ):
         self.base_path = path
         self.enable_filter = enable_filter
-        if not os.path.exists(path):
-            os.makedirs(path)
-        if not os.path.exists(os.path.join(path, "id_meta.json")):
-            cloud_file = get_file("output/id_meta.json")
-            with open(os.path.join(path, "id_meta.json"), "wb") as f:
-                f.write(cloud_file.read())
-        with open(os.path.join(path, "id_meta.json")) as f:
-            self.meta_file = json.load(f)
-        self.data_items = self.get_data_items(path)
-        self.condition_max_size = condition_max_size
-        self.to_tensor = T.ToTensor()
-
-    def get_data_items(self, path):
-        data_items = []
-        for idx in self.meta_file:
-            group_idx = "/".join(idx.split("/")[:-1])
-            item_idx = idx.split("/")[-1].split(".")[0]
-            valid_c = self.meta_file[idx]
-            valid = (
-                valid_c["compositeStructure"] > 4
-                and valid_c["objectConsistency"] > 4
-                and valid_c["imageQuality"] > 4
-            )
-            if not valid and self.enable_filter:
-                continue
-            data_items.append((group_idx, item_idx))
-        return data_items
-
-    def __len__(self):
-        return len(self.data_items)
-
-    def __getitem__(self, idx):
-        img_size = 512
-        padding = 8
-
-        sample_meta = self.data_items[idx]
-
-        image_path = os.path.join(
-            self.base_path, sample_meta[0], f"{sample_meta[1]}.jpg"
-        )
-        meta_path = os.path.join(self.base_path, sample_meta[0], f"meta.json")
-        if not os.path.exists(image_path):
-            if not os.path.exists(os.path.join(self.base_path, sample_meta[0])):
-                os.makedirs(os.path.join(self.base_path, sample_meta[0]))
-            cloud_file = get_file(f"output/{sample_meta[0]}/{sample_meta[1]}.jpg")
-            with open(image_path, "wb") as f:
-                f.write(cloud_file.read())
-        if not os.path.exists(meta_path):
-            cloud_file = get_file(f"output/{sample_meta[0]}/meta.json")
-            with open(meta_path, "wb") as f:
-                f.write(cloud_file.read())
-
-        image = Image.open(image_path).convert("RGB")
-        with open(meta_path) as f:
-            meta = json.load(f)
-
-        left_img = image.crop(
-            (padding, padding, img_size + padding, img_size + padding)
-        )
-        right_img = image.crop(
-            (
-                img_size + padding * 2,
-                padding,
-                img_size * 2 + padding * 2,
-                img_size + padding,
-            )
-        )
-        descriptions = meta["description"]["scenes_descriptions"] + [
-            meta["description"]["studio_description"]
-        ]
-        description1 = descriptions[int(sample_meta[1].split("_")[0])]
-        description2 = descriptions[int(sample_meta[1].split("_")[1])]
-
-        # image_pair = cat images
-        image_pair = Image.new("RGB", (img_size * 2, img_size))
-        image_pair.paste(left_img, (0, 0))
-        image_pair.paste(right_img, (img_size, 0))
-
-        return {
-            "image1": left_img,
-            "image2": right_img,
-            "description1": description1,
-            "description2": description2,
-            "instance": meta["instance"],
-            "image_pair": image_pair,
-        }
-
-
-class Split2(Dataset):
-    def __init__(
-        self,
-        path: str,
-        condition_max_size: int = 512,
-        enable_filter: bool = True,
-    ):
-        self.base_path = path
-        self.enable_filter = enable_filter
+        self.prefix = prefix
 
         if not os.path.exists(path):
             os.makedirs(path)
         if not os.path.exists(os.path.join(path, "id_meta.json")):
-            cloud_file = get_file("output_new/id_meta_new.json")
+            cloud_file = get_file(
+                f"{prefix}/{'id_meta_new.json' if prefix == 'output_new' else 'id_meta.json'}"
+            )
             with open(os.path.join(path, "id_meta.json"), "wb") as f:
                 f.write(cloud_file.read())
         with open(os.path.join(path, "id_meta.json")) as f:
@@ -150,10 +57,9 @@ class Split2(Dataset):
             group_idx = "/".join(idx.split("/")[:-1])
             item_idx = idx.split("/")[-1].split(".")[0]
             valid_c = self.meta_file[idx]
-            valid = (
-                valid_c["compositeStructure"] > 4
-                and valid_c["objectConsistency"] > 4
-                and valid_c["imageQuality"] > 4
+            valid = all(
+                valid_c[key] > 4
+                for key in ["compositeStructure", "objectConsistency", "imageQuality"]
             )
             if not valid and self.enable_filter:
                 continue
@@ -166,26 +72,27 @@ class Split2(Dataset):
     def __getitem__(self, idx):
         img_size = 512
         padding = 8
-
         sample_meta = self.data_items[idx]
 
-        image_path = os.path.join(
-            self.base_path, sample_meta[0], f"{sample_meta[1]}.jpg"
-        )
-        meta_path = os.path.join(self.base_path, sample_meta[0], f"meta.json")
-        if not os.path.exists(image_path):
-            if not os.path.exists(os.path.join(self.base_path, sample_meta[0])):
-                os.makedirs(os.path.join(self.base_path, sample_meta[0]))
-            cloud_file = get_file(f"output_new/{sample_meta[0]}/{sample_meta[1]}.jpg")
-            with open(image_path, "wb") as f:
-                f.write(cloud_file.read())
-        if not os.path.exists(meta_path):
-            cloud_file = get_file(f"output_new/{sample_meta[0]}/meta.json")
-            with open(meta_path, "wb") as f:
-                f.write(cloud_file.read())
+        paths = {
+            "image": os.path.join(
+                self.base_path, sample_meta[0], f"{sample_meta[1]}.jpg"
+            ),
+            "meta": os.path.join(self.base_path, sample_meta[0], "meta.json"),
+        }
 
-        image = Image.open(image_path).convert("RGB")
-        with open(meta_path) as f:
+        for key, path in paths.items():
+            if not os.path.exists(path):
+                if key == "image" and not os.path.exists(os.path.dirname(path)):
+                    os.makedirs(os.path.dirname(path))
+                cloud_file = get_file(
+                    f"{self.prefix}/{sample_meta[0]}/{os.path.basename(path)}"
+                )
+                with open(path, "wb") as f:
+                    f.write(cloud_file.read())
+
+        image = Image.open(paths["image"]).convert("RGB")
+        with open(paths["meta"]) as f:
             meta = json.load(f)
 
         left_img = image.crop(
@@ -200,14 +107,45 @@ class Split2(Dataset):
             )
         )
 
-        descriptions = meta["scene_descriptions"] + [meta["studio_photo_description"]]
-        description1 = descriptions[int(sample_meta[1].split("_")[0])]
-        description2 = descriptions[int(sample_meta[1].split("_")[1])]
-
         image_pair = Image.new("RGB", (img_size * 2, img_size))
         image_pair.paste(left_img, (0, 0))
         image_pair.paste(right_img, (img_size, 0))
 
+        return self.get_return_dict(meta, left_img, right_img, image_pair, sample_meta)
+
+
+class Split1(SplitBase):
+    def __init__(
+        self, path: str, condition_max_size: int = 512, enable_filter: bool = True
+    ):
+        super().__init__(path, condition_max_size, enable_filter, "output")
+
+    def get_return_dict(self, meta, left_img, right_img, image_pair, sample_meta):
+        descriptions = meta["description"]["scenes_descriptions"] + [
+            meta["description"]["studio_description"]
+        ]
+        description1 = descriptions[int(sample_meta[1].split("_")[0])]
+        description2 = descriptions[int(sample_meta[1].split("_")[1])]
+        return {
+            "image1": left_img,
+            "image2": right_img,
+            "description1": description1,
+            "description2": description2,
+            "instance": meta["instance"],
+            "image_pair": image_pair,
+        }
+
+
+class Split2(SplitBase):
+    def __init__(
+        self, path: str, condition_max_size: int = 512, enable_filter: bool = True
+    ):
+        super().__init__(path, condition_max_size, enable_filter, "output_new")
+
+    def get_return_dict(self, meta, left_img, right_img, image_pair, sample_meta):
+        descriptions = meta["scene_descriptions"] + [meta["studio_photo_description"]]
+        description1 = descriptions[int(sample_meta[1].split("_")[0])]
+        description2 = descriptions[int(sample_meta[1].split("_")[1])]
         return {
             "image1": left_img,
             "image2": right_img,
